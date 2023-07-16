92efe53be09216454bc1f5565dbcdf1b355aec7d
John Ovalle <108074536+John-TIC@users.noreply.github.com>
2023-07-16 14:40:06 -0500

    Add files via upload

---
+++ Tema7A/Obc_Ejercicio_7_1.py
@@ -0,0 +1,10 @@
+import Obc_Operaciones
+
+def main():
+    print(Obc_Operaciones.sumar(8,4))
+    print(Obc_Operaciones.restar(8,4))
+    print(Obc_Operaciones.multiplicar(8,4))
+    print(Obc_Operaciones.dividir(8,4))
+
+if __name__ == '__main__':
+    main()
